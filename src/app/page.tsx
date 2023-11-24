e1472905c0bb8fa52e79968ae10b48c7f74d9502
cheeseNA <kurocat2000@gmail.com>
2023-11-24 03:22:42 +0100

    Add CheeseScene

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { CheeseScene } from "@/components/Cheese";
+import { Canvas } from "@react-three/fiber";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="flex flex-row justify-center min-h-screen py-20">
+        <div id="canvas-container">
+          <Canvas>
+            <CheeseScene />
+          </Canvas>
+        </div>
+      </div>
+    </main>
+  );
 }
