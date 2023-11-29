46cbb26c002f146a693d285a1c809e0b57f19fda
cheeseNA <kurocat2000@gmail.com>
2023-11-29 02:15:27 +0100

    Add orbit controls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CheeseScene } from "@/components/Cheese";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Image from "next/image";
 
@@ -8,9 +9,15 @@ export default function Home() {
   return (
     <main>
       <div className="flex flex-row justify-center min-h-screen py-20">
-        <div id="canvas-container">
-          <Canvas>
+        <div id="canvas-container" className="w-full">
+          <Canvas shadows>
             <CheeseScene />
+            <OrbitControls
+              enablePan={false}
+              enableZoom={false}
+              maxPolarAngle={Math.PI / 2}
+              minPolarAngle={Math.PI / 2}
+            />
           </Canvas>
         </div>
       </div>
