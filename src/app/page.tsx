aa846978cca15111a9a774d7b9b6cc0b52978b82
cheeseNA <kurocat2000@gmail.com>
2023-11-30 01:20:00 +0100

    Update to render CheeseView

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
 "use client";
 
-import { CheeseScene } from "@/components/Cheese";
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import Image from "next/image";
+import { CheeseView } from "@/components/CheeseView";
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-row justify-center min-h-screen py-20">
-        <div id="canvas-container" className="w-full">
-          <Canvas shadows>
-            <CheeseScene />
-            <OrbitControls
-              enablePan={false}
-              enableZoom={false}
-              maxPolarAngle={Math.PI / 2}
-              minPolarAngle={Math.PI / 2}
-            />
-          </Canvas>
-        </div>
+        <CheeseView />
       </div>
     </main>
   );
