5c6a4af7047232fd6c7a8caaaeb226d99e972859
cheeseNA <kurocat2000@gmail.com>
2023-12-10 14:24:20 +0100

    Add template about section from flowrift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default function Home() {
   return (
     <main>
-      <div className="bg-white pb-6 sm:pb-8 lg:pb-12 pt-60">
+      <div className="bg-white pb-6 sm:pb-8 lg:pb-12 pt-60" id="top-section">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
           <section className="flex flex-col justify-between gap-6 sm:gap-10 md:gap-16 lg:flex-row">
             <div className="flex flex-col items-center justify-center sm:text-center lg:items-start lg:py-12 lg:text-left xl:w-5/12 xl:py-24">
@@ -37,6 +37,80 @@ export default function Home() {
               <CheeseView />
             </div>
           </section>
+        </div>
+      </div>
+
+      <div className="bg-white py-6 sm:py-8 lg:py-12">
+        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+          <div className="mb-10 md:mb-16">
+            <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+              Selected
+            </h2>
+
+            <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+              This is a section of some simple filler text, also known as
+              placeholder text. It shares some characteristics of a real written
+              text but is random or otherwise generated.
+            </p>
+          </div>
+          <div className="grid gap-8 md:grid-cols-2 lg:gap-12">
+            <div>
+              <div className="h-64 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-auto">
+                <img
+                  src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600&h=750"
+                  loading="lazy"
+                  alt="Photo by Martin Sanchez"
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            </div>
+
+            <div className="md:pt-8">
+              <p className="text-center font-bold text-indigo-500 md:text-left">
+                Who we are
+              </p>
+
+              <h1 className="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6 md:text-left">
+                Our competitive advantage
+              </h1>
+
+              <p className="mb-6 text-gray-500 sm:text-lg md:mb-8">
+                This is a section of some simple filler text, also known as
+                placeholder text. It shares some characteristics of a real
+                written text but is random or otherwise generated. It may be
+                used to display a sample of fonts or generate text for testing.
+                Filler text is dummy text which has no meaning however looks
+                very similar to real text.
+                <br />
+                <br />
+                This is a section of some simple filler text, also known as
+                placeholder text. It shares some characteristics of a real
+                written text but is{" "}
+                <a
+                  href="#"
+                  className="text-indigo-500 underline transition duration-100 hover:text-indigo-600 active:text-indigo-700"
+                >
+                  random
+                </a>{" "}
+                or otherwise generated. It may be used to display a sample of
+                fonts or generate text for testing. Filler text is dummy text
+                which has no meaning however looks very similar to real text.
+              </p>
+
+              <h2 className="mb-2 text-center text-xl font-semibold text-gray-800 sm:text-2xl md:mb-4 md:text-left">
+                About us
+              </h2>
+
+              <p className="mb-6 text-gray-500 sm:text-lg md:mb-8">
+                This is a section of some simple filler text, also known as
+                placeholder text. It shares some characteristics of a real
+                written text but is random or otherwise generated. It may be
+                used to display a sample of fonts or generate text for testing.
+                Filler text is dummy text which has no meaning however looks
+                very similar to real text.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </main>
